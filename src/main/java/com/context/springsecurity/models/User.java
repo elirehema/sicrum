8992f939deca85ecc14ed55e3a5b21f311dc9de0
=elirehema <=rocksandystone@gmail.com>
2020-06-23 20:30:21 +0300

    component updates

--- src/main/java/com/context/springsecurity/models/User.java
+++ src/main/java/com/context/springsecurity/models/User.java
@@ -1,112 +1,89 @@
 package com.context.springsecurity.models;
 
-import org.springframework.stereotype.Repository;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
-import java.util.HashSet;
-import java.util.Set;
-
-/**
- * -- This file created by eli on 16/06/2020 for springsecurity
- * --
- * -- Licensed to the Apache Software Foundation (ASF) under one
- * -- or more contributor license agreements. See the NOTICE file
- * -- distributed with this work for additional information
- * -- regarding copyright ownership. The ASF licenses this file
- * -- to you under the Apache License, Version 2.0 (the
- * -- "License"); you may not use this file except in compliance
- * -- with the License. You may obtain a copy of the License at
- * --
- * -- http://www.apache.org/licenses/LICENSE-2.0
- * --
- * -- Unless required by applicable law or agreed to in writing,
- * -- software distributed under the License is distributed on an
- * -- "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * -- KIND, either express or implied. See the License for the
- * -- specific language governing permissions and limitations
- * -- under the License.
- * --
- **/
 
 @Entity
-@Table(name = "users",
-        uniqueConstraints = {
-                @UniqueConstraint(columnNames = "username"),
-                @UniqueConstraint(columnNames = "email")
-        })
+@Table(	name = "users", 
+		uniqueConstraints = { 
+			@UniqueConstraint(columnNames = "username"),
+			@UniqueConstraint(columnNames = "email") 
+		})
 public class User {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-    @NotBlank
-    @Size(max = 20)
-    private String username;
+	@NotBlank
+	@Size(max = 20)
+	private String username;
 
-    @NotBlank
-    @Size(max = 50)
-    @Email
-    private String email;
+	@NotBlank
+	@Size(max = 50)
+	@Email
+	private String email;
 
-    @NotBlank
-    @Size(max = 120)
-    private String password;
+	@NotBlank
+	@Size(max = 120)
+	private String password;
 
-    @ManyToMany(fetch = FetchType.LAZY)
-    @JoinTable(name = "user_roles",
-            joinColumns = @JoinColumn(name = "user_id"),
-            inverseJoinColumns = @JoinColumn(name = "role_id"))
-    private Set<Role> roles = new HashSet<>();
+	@ManyToMany(fetch = FetchType.LAZY)
+	@JoinTable(	name = "user_roles", 
+				joinColumns = @JoinColumn(name = "user_id"), 
+				inverseJoinColumns = @JoinColumn(name = "role_id"))
+	private Set<Role> roles = new HashSet<>();
 
-    public User() {
-    }
+	public User() {
+	}
 
-    public User(String username, String email, String password) {
-        this.username = username;
-        this.email = email;
-        this.password = password;
-    }
+	public User(String username, String email, String password) {
+		this.username = username;
+		this.email = email;
+		this.password = password;
+	}
 
-    public Long getId() {
-        return id;
-    }
+	public Long getId() {
+		return id;
+	}
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    public String getUsername() {
-        return username;
-    }
+	public String getUsername() {
+		return username;
+	}
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
+	public void setUsername(String username) {
+		this.username = username;
+	}
 
-    public String getEmail() {
-        return email;
-    }
+	public String getEmail() {
+		return email;
+	}
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
-    public String getPassword() {
-        return password;
-    }
+	public String getPassword() {
+		return password;
+	}
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
-    public Set<Role> getRoles() {
-        return roles;
-    }
+	public Set<Role> getRoles() {
+		return roles;
+	}
 
-    public void setRoles(Set<Role> roles) {
-        this.roles = roles;
-    }
+	public void setRoles(Set<Role> roles) {
+		this.roles = roles;
+	}
 }
--- src/main/java/com/context/springsecurity/security/jwt/AuthEntryPointJwt.java
+++ src/main/java/com/context/springsecurity/security/jwt/AuthEntryPointJwt.java
@@ -1,47 +1,27 @@
-package com.context.springsecurity.security.services.jwt;
+package com.context.springsecurity.security.jwt;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.web.AuthenticationEntryPoint;
-import org.springframework.stereotype.Component;
+import java.io.IOException;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
-/**
- * -- This file created by eli on 16/06/2020 for springsecurity
- * --
- * -- Licensed to the Apache Software Foundation (ASF) under one
- * -- or more contributor license agreements. See the NOTICE file
- * -- distributed with this work for additional information
- * -- regarding copyright ownership. The ASF licenses this file
- * -- to you under the Apache License, Version 2.0 (the
- * -- "License"); you may not use this file except in compliance
- * -- with the License. You may obtain a copy of the License at
- * --
- * -- http://www.apache.org/licenses/LICENSE-2.0
- * --
- * -- Unless required by applicable law or agreed to in writing,
- * -- software distributed under the License is distributed on an
- * -- "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * -- KIND, either express or implied. See the License for the
- * -- specific language governing permissions and limitations
- * -- under the License.
- * --
- **/
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
 @Component
 public class AuthEntryPointJwt implements AuthenticationEntryPoint {
 
-    private static final Logger logger = LoggerFactory.getLogger(AuthEntryPointJwt.class);
+	private static final Logger logger = LoggerFactory.getLogger(AuthEntryPointJwt.class);
 
-    @Override
-    public void commence(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AuthenticationException authenticationException) throws IOException, ServletException {
+	@Override
+	public void commence(HttpServletRequest request, HttpServletResponse response,
+			AuthenticationException authException) throws IOException, ServletException {
+		logger.error("Unauthorized error: {}", authException.getMessage());
+		response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Error: Unauthorized");
+	}
 
-        logger.error("Unauthorized error: {}", authenticationException.getMessage());
-        httpServletResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Error: Unauthorized");
-    }
 }
